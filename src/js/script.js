933df44feb1ad0d55ff4350e46041f0c8c716a08
Gouraud Luna <luna.gouraud@edu.univ-fcomte.fr>
2025-11-07 10:46:23 +0100

    final

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,11 @@
 // TODO
+const metrouge = document.querySelector('#a-mettre-en-rouge');
+metrouge.style.color = 'red';
+const clickrouge = document.querySelector('#en-rouge-suite-a-click');
+clickrouge.addEventListener("click",()=>clickrouge.style.color = 'red');
+const ttH2 = document.querySelectorAll('h2');
+ttH2.forEach((elm)=>{
+    elm.addEventListener("click", (evt)=>{
+        evt.target.style.color = 'red';
+    });
+});
